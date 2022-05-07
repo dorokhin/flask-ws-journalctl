7030e78c77abb37e5dbd40c4e8cb98821b11fa41
deepblack <andrewnorilsk@gmail.com>
2022-05-07 23:24:38 +0300

    initial commt

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template
+from flask_sock import Sock
+import subprocess
+import json
+
+app = Flask(__name__)
+sock = Sock(app)
+app.config['SOCK_SERVER_OPTIONS'] = {'ping_interval': 25}
+
+
+@app.route('/')
+def index():
+    data = {
+        'page_title': 'ws-example',
+        'ws_url': 'ws://127.0.0.1:5000/log'
+    }
+    return render_template('base.html', data=data)
+
+
+@sock.route('/echo')
+def echo(ws):
+    while True:
+        data = ws.receive()
+        ws.send(data)
+        print(data)
+
+
+@sock.route('/log')
+def log(ws):
+    data = ws.receive()
+    payload = json.loads(data)
+
+    with subprocess.Popen(
+            [f'journalctl -u {payload["name"]} -f'],
+            stdout=subprocess.PIPE, shell=True, bufsize=1,
+            universal_newlines=True
+    ) as process:
+        print('subprocess executed')
+        for line in process.stdout:
+            line = line.rstrip()
+            print(line)
+            try:
+                payload = {
+                    'title': 'journalctl',
+                    'message': line,
+                    'success': True
+                }
+                ws.send(json.dumps(payload))
+            except BaseException as e:
+                payload = {
+                    'error': str(e) + '\n',
+                    'success': False
+                }
+                ws.send(payload)
+    print('WS closed')
+
+
+if __name__ == '__main__':
+    app.run()
